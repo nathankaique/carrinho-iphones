5cf92640f581ab96f8010cda514b9393734ac697
nathankaique <119627814+nathankaique@users.noreply.github.com>
2024-10-15 16:10:54 -0300

    Add files via upload

---
+++ carrinho de compras-iphone/pizzas.js
@@ -0,0 +1,100 @@
+let pizzaJson = [
+    {
+        id: 1,
+        name: 'iphone 11',
+        img: 'images/iphone_11-removebg-preview__2___1_-removebg-preview.png',
+        price: [ 18.00, 20.00, 23.00],
+        sizes: [
+          'modelo 1',
+          'modelo 2',
+          'modelo 3'
+        ],
+         description: 'capinhas fortes e resistentes'
+        
+      },
+      {
+        id: 2,
+        name: 'iphone 12',
+        img: 'images/iphone12-removebg-preview.png',
+        price: [21.00, 24.00, 26.00],
+        sizes: [
+          'modelo 1',
+          'modelo 2',
+          'modelo 3'
+        ],
+        description: 'capinhas fortes e resistentes'
+      },
+      {
+        id: 3,
+        name: 'iphone 13 Mini',
+        img: 'images/iphone13-removebg-preview.png',
+        price: [23.00, 26.00, 28.00],
+        sizes: [
+          'modelo 1',
+          'modelo 2',
+          'modelo 3'
+        ],
+        description: 'capinhas fortes e resistentes'
+      },
+      {
+        id: 4,
+        name: 'iphone 13',
+        img: 'images/1520-1__1_-removebg-preview.png',
+        price: [25.00, 26.00, 28.00],
+        sizes: [
+          'modelo 1',
+          'modelo 2',
+          'modelo 3'
+        ],
+        
+        description: 'capinhas fortes e resistentes'
+      },
+      {
+        id: 5,
+        name: 'Iphone 14 Plus',
+        img: 'images/iphone14plus__1_-removebg-preview.png',
+        price: [24.00, 27.00, 29.00],
+        sizes: [
+          'modelo 1',
+          'modelo 2',
+          'modelo 3'
+        ],
+        description: 'capinhas fortes e resistentes'
+      },
+      {
+        id: 6,
+        name: 'iphone 14 ',
+        img: 'images/iphone14__1_-removebg-preview.png',
+        price: [30.00, 33.00, 35.00],
+        sizes: [
+          'modelo 1',
+          'modelo 2',
+          'modelo 3'
+        ],
+        description: 'capinhas fortes e resistentes'
+      },
+      {
+        id: 7,
+        name: 'Iphone 15',
+        img: 'images/iphone15-removebg-preview.png',
+        price: [27.00, 30.00, 32.00],
+        sizes: [
+          'modelo 1',
+          'modelo 2',
+          'modelo 3'
+        ],
+        description: 'capinhas fortes e resistentes'
+      },
+      {
+        id: 8,
+        name: 'Iphone 16',
+        img: 'images/iphone166-removebg-preview.png',
+        price: [30.00, 32.00, 35.00],
+        sizes: [
+          'modelo 1',
+          'modelo 2',
+          'modelo 3'
+        ],
+        description: 'capinhas fortes e resistentes'
+      }
+];
